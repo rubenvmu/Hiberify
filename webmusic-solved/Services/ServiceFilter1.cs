5a7af7034c26208efebf4b435c1fed8e8f7de0ac
rvmu <rvmu@rvmu.es>
2024-06-04 14:50:01 +0200

    service

--- webmusic-solved/Services/ServiceFilter1.cs
+++ webmusic-solved/Services/ServiceFilter1.cs
@@ -1,6 +1,9 @@
-﻿namespace webmusic_solved.Services
+﻿using System.Drawing;
+
+namespace webmusic_solved.Services
 {
     public class ServiceFilter1
     {
+        public string[] orden { get; set; }
     }
 }
