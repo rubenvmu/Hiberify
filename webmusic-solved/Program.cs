1b19918f690fa8ec72d2a1269b2b29f18d238209
rvmu <rvmu@rvmu.es>
2024-06-04 12:59:42 +0200

    solved Albumes

--- webmusic-solved/Program.cs
+++ webmusic-solved/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using webmusic_solved.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<GrupoAContext>(
+    options => options.UseSqlServer("server=musicagrupos.databaase.windows.net;database=GrupoA;user=as;password=P0t@t0P0t@t0"));
 
 var app = builder.Build();
 
