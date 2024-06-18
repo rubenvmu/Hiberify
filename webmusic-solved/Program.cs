cbbc960af9b6683d715a98cd8d863a9942d6052a
rvmu <rvmu@rvmu.es>
2024-06-18 12:13:06 +0200

    Parrot retry

--- webmusic-solved/Program.cs
+++ webmusic-solved/Program.cs
@@ -6,8 +6,17 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<GrupoAContext>
-    (options => options.UseSqlServer("'server=musicagrupos.databaase.windows.net;database=GrupoA;Integrated Security=True"));
+builder.Services.AddDbContext<GrupoAContext>(options =>
+{
+    options.UseSqlServer("server=musicagrupos.databaase.windows.net;database=GrupoA;Integrated Security=True",
+        sqlServerOptionsAction: sqlOptions =>
+        {
+            sqlOptions.EnableRetryOnFailure(
+                maxRetryCount: 5,
+                maxRetryDelay: TimeSpan.FromSeconds(60),
+                errorNumbersToAdd: null);
+        });
+});
 builder.Services.AddScoped<IAlbumService, AlbumService>();
 builder.Services.AddScoped<ICancionesService, CancionesService>();
 builder.Services.AddScoped<IAlbumesRepositorio, fakeAlbumesRepositorio>();
