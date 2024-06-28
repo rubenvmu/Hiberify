fbe63b2dce7cbaa266bf2b91e17205ffda0dddf5
rvmu <rvmu@rvmu.es>
2024-06-28 14:12:10 +0200

    A ver si buildea ahora

--- webmusic-solved/Models/GrupoAContext.cs
+++ webmusic-solved/Models/GrupoAContext.cs
@@ -25,7 +25,6 @@ public partial class GrupoAContext : DbContext
     public virtual DbSet<Festival> Festival { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlServer("server=musicagrupos.database.windows.net;database=GrupoA;user=as;password=P0t@t0P0t@t0");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
