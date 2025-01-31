09ba54d7e9d1a1634cd16c7ff299b36420de0c2e
Ruben Vasile Marcu Ungureanu <rvmu@rvmu.es>
2025-02-01 00:01:12 +0100

    abandoned

--- hiberify/Program.cs
+++ hiberify/Program.cs
@@ -22,11 +22,9 @@ builder.Services.AddScoped<ICancionesService, CancionesService>();
 builder.Services.AddScoped<ICountersqlService, CountersqlService>();
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
